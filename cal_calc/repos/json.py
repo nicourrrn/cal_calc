3abc3ae2d087b8b271cf1218ba3899d25dd08cfb
nicourrrn <s57111702@gmail.com>
2023-07-14 22:31:39 +0300

    Create json repo

--- cal_calc/repos/json.py
+++ cal_calc/repos/json.py
@@ -1,1 +1,17 @@
+from cal_calc.models import Day
+import base
+import json
+
+class JsonRepo(base.AbstractBaseRepo):
+    def __init__(self, filename: str = "days.json"):
+        self.file = open(filename, 'rw')
+        
+    def loads(self) -> list[Day]:
+        try: content: str = self.file.read()
+        except FileExistsError:
+            raise Exception("Invalid file") 
+        return [Day(**d) for d in json.loads(content)]
+        
+    def dumps(self, days: list[Day]):
+        json.dumps(days, default=lambda d: d.__dict__) 
 
